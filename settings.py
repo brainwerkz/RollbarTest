5fc91800c26010a5b5ebf8ffa37b8e27ee307e9e
jrichardson78745 <jrichardson@brainwerkz.com>
2017-01-19 21:20:54 -0600

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,6 @@
+ROLLBAR = {
+    'access_token': '8968e0219c5342bd96b76b8c1cab8bee',
+    'environment': 'development' if DEBUG else 'production',
+    'branch': 'master',
+    'root': '/absolute/path/to/code/root',
+}
